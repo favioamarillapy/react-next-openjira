6ff2d184e22adb2b2dc2dd7b2b7c085c2f4212a7
favioamarillapy <53306199+favioamarillapy@users.noreply.github.com>
2022-03-22 15:44:06 -0300

    Configurated themes

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,7 +1,18 @@
 import '../styles/globals.css'
 
+import { CssBaseline, ThemeProvider } from '@mui/material'
+
+import { darkTheme } from '../themes'
+
+
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
 
 export default MyApp
